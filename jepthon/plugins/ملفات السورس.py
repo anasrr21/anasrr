530dec41cc2a2ef22b6d09d75d4a71d5dd52a5a8
♥امير <113321419+abdalalaem@users.noreply.github.com>
2022-11-04 02:12:01 +0300

    Update ملفات السورس.py

--- jepthon/plugins/ملفات السورس.py
+++ jepthon/plugins/ملفات السورس.py
@@ -15,7 +15,7 @@ async def reda(event):
     list = "**قائمة المكاتب المثبته**\n"
     for i in installed_packages_list:
         list += f"{i}\n"
-    list += "**سورس الجوكر**"
+    list += "**سورس سيرثون**"
     await edit_or_reply(event, list)
 
 @jepiq.ar_cmd(
@@ -30,7 +30,7 @@ async def _(event):
     "To list all plugins in jepthon"
     cmd = "ls jepthon/plugins"
     o = (await _catutils.runcmd(cmd))[0]
-    OUTPUT = f"**[الجوكر](tg://need_update_for_some_feature/) الـمـلفـات:**\n{o}"
+    OUTPUT = f"**[سيرثون](tg://need_update_for_some_feature/) الـمـلفـات:**\n{o}"
     await edit_or_reply(event, OUTPUT)
 
 
@@ -48,7 +48,7 @@ async def _(event):
     cmd = "env"
     o = (await _catutils.runcmd(cmd))[0]
     OUTPUT = (
-        f"**[الجوكر](tg://need_update_for_some_feature/) قـائمـة الـفـارات:**\n\n\n{o}\n\n**انتبه هنالك معلومات حساسة لا تُعطِها لشخص غير موثوق**"
+        f"**[سيرثون](tg://need_update_for_some_feature/) قـائمـة الـفـارات:**\n\n\n{o}\n\n**انتبه هنالك معلومات حساسة لا تُعطِها لشخص غير موثوق**"
     )
     await edit_or_reply(event, "**تم ارسال المعلومات في الرسائل المحفوضة \nانتبه من الاشخاص الي يطلبون منك كتابة هذا الامر يريد ان يخترقك!**")
     await jepiq.send_message("me", OUTPUT)
